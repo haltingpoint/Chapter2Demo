6fa02d74ffca56b61f3f405756827f0b64bae4f3
Haltingpoint <haltingpoint@gmail.com>
2013-05-21 17:38:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chapter2Demo::Application.config.secret_token = '97fa7f3211af605a3cfc9e81d883de9eec98b74898b0ff3c1bfb7dea875397295ecea1b40bc93d1a9bf9aa9f287bb986854482c41ba102cfba721433a0742063'
